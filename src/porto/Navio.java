69b62b36b99d5a5dbc5608d32a21d1e35f3bb4cc
35191 <35191@LAPTOP-U574CDT5>
2020-03-11 12:51:58 +0000

    add classes e variáveis

---
+++ src/porto/Navio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package porto;
+
+/**
+ *
+ * @author 35191
+ */
+public class Navio {
+    private String matNavio;
+    private String name;
+    private float comp; 
+    
+}
---
+++ src/porto/PortaContentores.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package porto;
+
+/**
+ *
+ * @author 35191
+ */
+public class PortaContentores {
+    private int nmrMaxCont; 
+}
